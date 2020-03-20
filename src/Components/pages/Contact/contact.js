480c21ed8a80385526221470da735a924b9af05e
samaromm <samaromm98@gmail.com>
2020-03-20 03:40:21 +0300

    changing font color

--- src/Components/pages/Contact/contact.js
+++ src/Components/pages/Contact/contact.js
@@ -11,7 +11,7 @@ export default class contact extends React.Component{
             <MDBContainer>
                 <MDBCard className="text-center">
                     <MDBCardBody className="border border-white  scrollbar scrollbar-secondary" style={scrollContainerStyle}>
-                        <h2 className="h1-responsive font-weight-bold text-center ">Contact me</h2>
+                        <h2 className="h1-responsive font-weight-bold text-center text-secondary ">Contact me</h2>
                         <MDBRow>
                             <MDBCol md="9" className="md-0 mb-5">
                                 <Form/>
