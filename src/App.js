dc1b83453ee5b07e8fd5dabdc98b4afeb29315d1
samaromm <samaromm98@gmail.com>
2020-03-21 03:45:13 +0300

    hash router

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { HashRouter, Route } from 'react-router-dom';
 import Header from './Components/Header/Header.js'
 import About from './Components/pages/About/About';
 import Contact from './Components/pages/Contact/contact';
@@ -14,7 +14,7 @@ class App extends React.Component{
   render(){
     return(
       <div className="bg">
-        <Router>
+        <HashRouter basename='/'>
            <Header/>
            <Route path='/' exact component={Home}/>
            <Route path='/about' exact component={About}/>
@@ -22,7 +22,7 @@ class App extends React.Component{
            <Route path='/cv' exact component={Cv}/>
            <Route path='/projects' exact component={Projects}/>
            <Footer/>
-        </Router>
+        </HashRouter>
       </div>  
     )
   }
