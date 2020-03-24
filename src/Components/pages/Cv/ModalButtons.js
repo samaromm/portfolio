341603cb89b41aa5b93a11a6123dff3db58441c3
samaromm <samaromm98@gmail.com>
2020-03-24 05:47:40 +0300

    adjusting the background size

--- src/Components/pages/Cv/ModalButtons.js
+++ src/Components/pages/Cv/ModalButtons.js
@@ -33,26 +33,27 @@ render() {
       <MDBRow className="d-flex align-items-center bd-highlight mb-3 
       justify-content-center animated slideInRight" style={{ height: '100%'}}>
         <MDBCol>  
-          <MDBBtn onClick={this.open} id="info" className="p-0 bd-highlight border buttonRound" 
-          style={{width:"80px", height:"80px"}} gradient="blue">General Info</MDBBtn>
-
-          <MDBBtn onClick={this.open} id="Education" className="p-0 bd-highlight border buttonRound" 
-          style={{width:"80px", height:"80px"}} gradient="blue">Education</MDBBtn>
-          
-          <MDBBtn onClick={this.open} id="Certifications" className="p-0 bd-highlight border buttonRound" 
-          style={{width:"80px", height:"80px"}} gradient="blue" >certificates</MDBBtn>
-          
-          <MDBBtn onClick={this.open} id="Skills" className="p-0 bd-highlight border buttonRound" 
-          style={{width:"80px", height:"80px"}} gradient="blue">Skills</MDBBtn>
-          
-          <MDBBtn onClick={this.open} id="Experince" className="p-0 bd-highlight border buttonRound" 
-          style={{width:"80px", height:"80px"}} gradient="blue">Experince</MDBBtn>
-
-          
-          <MDBBtn className="p-0 bd-highlight border buttonRound" 
-          style={{width:"80px", height:"80px"}} gradient="blue">
-            <a href="https://www.google.com.tr/" color="white">Pdf Copy</a>
+          <MDBBtn onClick={this.open} id="info" className="p-0 bd-highlight border cvButtons forSpin" gradient="blue">
+            General Info
           </MDBBtn>
+          <MDBBtn onClick={this.open} id="Education" className="p-0 bd-highlight border cvButtons forSpin" gradient="blue">
+            Education
+          </MDBBtn>
+          <MDBBtn onClick={this.open} id="Certifications" className="p-0 bd-highlight border cvButtons forSpin" gradient="blue" >
+            certificates
+          </MDBBtn>
+          <MDBBtn onClick={this.open} id="Skills" className="p-0 bd-highlight border cvButtons forSpin" gradient="blue">
+            Skills
+          </MDBBtn>  
+          <MDBBtn onClick={this.open} id="Experince" className="p-0 bd-highlight border cvButtons forSpin" gradient="blue">
+            Experince
+          </MDBBtn>
+    
+          <MDBBtn className="p-0 bd-highlight border cvButtons forSpin" gradient="blue">
+            <a href="https://drive.google.com/file/d/1zADKPL18qmA6MV7GYwgTII0MhQU0xh5A/view" 
+            style={{color:"white"}} target="blank" >pdf Copy</a>
+          </MDBBtn>
+          
         </MDBCol>
       </MDBRow> 
       <Body id={this.state.id} modal={this.state.modal} close={this.close}/>
